2c7c5b0c82154e1a60f6e345a58dea0f2be55573
Anirudh <anirudh.das2903@outlook.in>
2024-05-02 23:22:38 +0530

    added custom sign-in/up pages using clerk

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
-import { clerkMiddleware , createRouteMatcher } from "@clerk/nextjs/server";
+import { auth, clerkMiddleware , createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const protectedRoutes = createRouteMatcher([
+  "/",
+  "/upcoming",
+  "/previous",
+  "/recordings",
+  "/personal-room",
+  "/meeting(.*)"
+]);
+
+export default clerkMiddleware((auth,req) => {
+  if (protectedRoutes(req)) auth().protect();
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
