9b41629801c9198a53011548f7dab7e53a81febb
Anirudh <anirudh.das2903@outlook.in>
2024-05-05 21:38:25 +0530

    added call controls and modified the room page

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
+import React, { useState } from 'react'
 import Loader from "@/components/Loader";
 import MeetingRoom from "@/components/MeetingRoom";
 import MeetingSetup from "@/components/MeetingSetup";
 import { useGetCallById } from "@/hooks/useGetCallById";
 import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
-import React, { useState } from 'react'
 
 const Meeting = ({ params: {id} }: { params: {id: string} }) => {
 
