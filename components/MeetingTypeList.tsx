bc1ee6d36dd8f2fb320e5dd74ff8b914e749ec5f
Anirudh <anirudh.das2903@outlook.in>
2024-05-05 09:06:52 +0530

    added custom hooks to get call details by id and created meeting setup & room page

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -6,20 +6,30 @@ import { useRouter } from "next/navigation";
 import MeetingModal from "./MeetingModal";
 import { useUser } from "@clerk/nextjs";
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useToast } from "@/components/ui/use-toast"
+
 
 const MeetingTypeList = () => {
 
     const router = useRouter();
+    const {user} = useUser();
+    const client = useStreamVideoClient();
+
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>();
     const [values, setValues] = useState({dateTime: new Date(), description: '', link: ''});
     const [callDetails, setCallDetails] = useState<Call>();
-    const {user} = useUser();
-    const client = useStreamVideoClient();
+    const { toast } = useToast()
+
 
     const createMeeting = async () => {
         if(!client || !user) return;
 
         try {
+          if(!values.dateTime) {
+            toast({title: "Please select a date and time for the meeting"})
+            return;
+          }
+
           const id = crypto.randomUUID();
           const call = client.call('default', id);
 
@@ -41,9 +51,10 @@ const MeetingTypeList = () => {
           if(!values.description) {
             router.push(`/meeting/${call.id}`);
           }
+          toast({title: "Meeting created successfully"})
 
         } catch (error) {
-            console.log(error)
+             toast({variant: "destructive",title: "Uh oh! Something went wrong."})
         }
     }
 
