3d39f0bfc8487855131107171b65dd9f6b418464
Anirudh <anirudh.das2903@outlook.in>
2024-05-07 21:39:06 +0530

    updates

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,13 +2,6 @@ import StreamVideoProvider from "@/providers/StreamClientProvider";
 import { Metadata } from "next";
 import React, { ReactNode } from 'react'
 
-export const metadata: Metadata = {
-  title: "Nexus",
-  description: "A Video Conferencing Platform built with Stream & Next.js",
-  icons: {
-    icon: "/icons/logo.svg"
-  }
-};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
